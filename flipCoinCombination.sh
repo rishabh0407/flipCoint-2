b430676ac64b365c01e3ae83e81a58afc5a9200c
rishabh0407 <rishabhinvincible123@gmail.com>
2021-02-26 11:37:58 +0530

    Alter the code for singlet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,11 +1,31 @@
 #!/bin/bash -x
 
-toss=$(( RANDOM % 2 ));
 isHead=1;
 isTail=0;
-if(( $toss == $isHead ));
-then
+counter=0;
+counter1=0;
+declare -A singlet
+for(( i=0; i<=10; i++ ))
+do
+  toss=$(( RANDOM % 2 ));
+  if(( $toss == $isHead ));
+  then
     echo "Head"
-else 
+    singlet[((counter++))]="Heads"
+  else 
     echo "Tails"
+    singlet[((counter1++))]="Tails"
+  fi
+done
+if(( $counter > $counter1 ));
+then 
+    diff=$(( $counter - $counter1 ));
+    sum=$(( $counter + $counter1 ));
+    percentage=$(( $(( $diff / $sum )) * 100 ));
+    echo $percentage
+else
+     diff=$(( $counter1 - $counter ));
+    sum=$(( $counter + $counter1 ));
+    percentage=$(( $(( $diff / $sum )) * 100 ));
+    echo $percentage
 fi
